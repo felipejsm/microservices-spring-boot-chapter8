a2711107f8556b5eaa70849863c4d6a4cb50b7c7
felipejsm <felipejsm41@gmail.com>
2020-08-03 21:30:33 -0300

    Message pub class

--- organization-service/src/main/java/com/thoughtmechanix/organizationservice/OrganizationServiceApplication.java
+++ organization-service/src/main/java/com/thoughtmechanix/organizationservice/OrganizationServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
 
 @SpringBootApplication
 @EnableEurekaClient
